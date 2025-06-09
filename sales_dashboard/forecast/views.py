511ab3eccd7a3606d1d416613971cd1a146d152e
samgator <sam.morsics@gmail.com>
2025-06-09 17:35:23 +0900

    create views and templates for displaying charts

--- sales_dashboard/forecast/views.py
+++ sales_dashboard/forecast/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import SalesForecast
 
-# Create your views here.
+def forecast_chart(request):
+    data = SalesForecast.objects.order_by('date')
+    labels = [entry.date.strftime('%Y-%m-%d') for entry in data]
+    values = [entry.predicted_sales for entry in data]
+
+    return render(request, 'forecast/chart.html', {
+        'labels': labels,
+        'values': values,
+    })
