56f1fe25bb2c5c504013ffb72320b6e2caf02f59
samgator <sam.morsics@gmail.com>
2025-06-09 17:51:15 +0900

    create homepage to switch between the two apps

--- sales_dashboard/forecast/views.py
+++ sales_dashboard/forecast/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import SalesForecast
 
+def homepage(request):
+    return render(request, 'homepage.html')
+
 def forecast_chart(request):
     data = SalesForecast.objects.order_by('date')
     labels = [entry.date.strftime('%Y-%m-%d') for entry in data]
--- sales_dashboard/sales_dashboard/urls.py
+++ sales_dashboard/sales_dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from forecast.views import homepage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('forecast.urls')),
+    path('',  homepage, name='homepage'),
+    path('forecast/', include('forecast.urls')),
 ]
