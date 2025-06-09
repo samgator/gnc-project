9864beae04b6546149e8e8822ba6a696b5421de5
samgator <sam.morsics@gmail.com>
2025-06-09 17:26:12 +0900

    Connection to database

--- sales_dashboard/forecast/models.py
+++ sales_dashboard/forecast/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class SalesForecast(models.Model):
+    date = models.DateField()
+    predicted_sales = models.FloatField()
+
+    def __str__(self):
+        return f'{self.date}: {self.predicted_sales}'
